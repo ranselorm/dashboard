8b386e77c6a91514b15536ff7d246d73ff56bbec
Selorm <ranselorm@outlook.com>
2023-10-24 15:04:23 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,69 @@ import avatar from "../data/avatar.jpg";
 import { Cart, Chat, Notification, UserProfile } from ".";
 import { StateContext } from "../contexts/ContextProvider";
 
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <button
+    type="button"
+    onClick={customFunc}
+    style={{ color }}
+    className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+  >
+    {icon}
+    <span
+      style={{ background: dotColor }}
+      className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+    />
+  </button>
+);
+
 const Navbar = () => {
   const { activeMenu, setActiveMenu } = useContext(StateContext);
-  return <nav className="flex justify-between p-2 md:mx-6">Navbar</nav>;
+  return (
+    <nav className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevState) => !prevState)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          customFunc={() => handleClick("chat")}
+          dotColor="#03c9d7"
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notification"
+          customFunc={() => handleClick("notification")}
+          dotColor="#03c9d7"
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <div
+          className="flex items-center gap-2 p-1 cursor-pointer rounded-full hover:bg-light-gray text-gray-400"
+          onClick={() => handleClick("userProfile")}
+        >
+          <div className="w-[30px] h-[30px] rounded-full overflow-hidden">
+            <img src={avatar} alt="" className="object-cover w-full h-full" />
+          </div>
+          <p className="text-[14px]">
+            Hi,
+            <span className="ml-1 font-bold">Ransel</span>
+          </p>
+          <MdKeyboardArrowDown />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import "./App.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 
