703aed526f89882df5b372f8c04b616c25d508e0
Selorm <ranselorm@outlook.com>
2023-11-09 10:52:58 +0000

    Add pagination and sorting

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -19,14 +19,31 @@ import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div className="m-2 md:m-4 p-2 md:p-10 rounded-3xl bg-white">
+    <div className="m-2 mt-10 md:m-3 p-2 md:p-10 rounded-3xl bg-white">
       <Header category="Page" title="Orders" />
-      <GridComponent id="gridComp" dataSource={ordersData}>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            PdfExport,
+            Edit,
+          ]}
+        />
       </GridComponent>
     </div>
   );
