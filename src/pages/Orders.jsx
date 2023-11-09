f921b58c50a6323c68fac903fb16e7a93f1a733e
Selorm <ranselorm@outlook.com>
2023-11-09 09:52:11 +0000

    Add table column

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, ordersGrid, contextMenuItems } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-  return <div>Orders Page</div>;
+  return (
+    <div className="m-2 md:m-4 p-2 md:p-10 rounded-3xl bg-white">
+      <Header category="Page" title="Orders" />
+      <div className="bg-red-300">
+        <GridComponent>
+          <ColumnsDirective>
+            {ordersGrid.map((item, index) => (
+              <ColumnDirective key={index} {...item} />
+            ))}
+          </ColumnsDirective>
+        </GridComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
